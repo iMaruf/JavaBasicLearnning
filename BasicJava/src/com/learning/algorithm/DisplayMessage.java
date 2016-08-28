aa0a78a72b49b2aa0faa28882867ae4ff05cea46
Maruf <kamruzzamanmaruf@gmail.com>
2016-08-28 12:41:06 +0600

    Added a file which will print 'Hello world and for loop function.'

---
+++ BasicJava/src/com/learning/algorithm/DisplayMessage.java
@@ -0,0 +1,31 @@
+package com.learning.algorithm;
+
+public class DisplayMessage {
+
+	/**
+	 * This is my first
+	 *  java program.
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java world.");
+		
+		int a = 9;
+		int b = 8;
+		int c = 7;
+		
+		int sum;
+		
+		sum = a+b+c;
+		
+		System.out.println(sum);
+		
+		int x;
+		
+		for (x=1; x<10; x++){
+			System.out.println(x);
+		}
+		
+	}
+
+}
