82aec25d3b98b67fe2be379952cdabe53e3bb4f5
Maruf <kamruzzamanmaruf@gmail.com>
2016-08-30 20:11:52 +0600

    Added a file which will take input from user and print the result.

---
+++ BasicJava/src/com/learning/algorithm/ConsoleCalculator2.java
@@ -0,0 +1,40 @@
+package com.learning.algorithm;
+
+import java.util.Scanner;
+
+public class ConsoleCalculator2 {
+
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter two numbers: ");
+		
+		Scanner cScanner = new Scanner(System.in);
+		
+		String numOneString = cScanner.next();
+		String numTwoString = cScanner.next();
+		
+		System.out.println("The first token : " + numOneString);
+		System.out.println("The first token : " + numTwoString);
+		
+		double a = Double.parseDouble(numOneString);
+		double b = Double.parseDouble(numTwoString);
+		
+		double sum = Math.addTwoNumbers(a, b);
+		double sub = Math.subtractSecondFormFirst(a, b);
+		double mul = Math.multiplyTwoNumbers(a, b);
+		double div = Math.divideFirstBySecond(a, b);
+		
+		
+		System.out.println("The sum of " + a + " and " + b + " is : " + sum);
+		System.out.println("The subtraction of " + a + " from " + b + " is : " + sub);
+		System.out.println("The multiplication of " + a + " and " + b + " is : " + mul);
+		System.out.println("The divition of " + a + " by " + b + " is : " + div);
+
+		
+		
+
+
+	}
+
+}
