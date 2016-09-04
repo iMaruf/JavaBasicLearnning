84fe09bc1b006d026df3b09e10d6b2763168b5a7
Maruf <kamruzzamanmaruf@gmail.com>
2016-09-04 21:47:51 +0600

    Created a new project.

---
+++ BasicJava/src/my/project/DayOne.java
@@ -0,0 +1,13 @@
+package my.project;
+
+public class DayOne {
+
+	public static void main(String[] args) {
+		
+		String myName;
+		myName = new String("ABCD");
+		
+		System.out.println(myName);
+	}
+
+}
