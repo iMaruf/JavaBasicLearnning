d41e56fbc49613cb9708c08bd14a9664d1fa7268
Maruf <kamruzzamanmaruf@gmail.com>
2016-09-04 22:41:57 +0600

    Added new method.

--- BasicJava/src/my/project/DayOne.java
+++ BasicJava/src/my/project/DayOne.java
@@ -5,9 +5,18 @@ public class DayOne {
 	public static void main(String[] args) {
 		
 		String myName;
-		myName = new String("ABCD");
+		myName = new String(" Aariz ");
 		
+		System.out.print("Previous method");
 		System.out.println(myName);
+		
+		println("This is new method: :)");
+		print(myName);
 	}
-
+	static void println(Object anyObject){
+		System.out.println(anyObject);
+	}
+	static void print(Object anyObject){
+		System.out.print(anyObject);
+	}
 }
