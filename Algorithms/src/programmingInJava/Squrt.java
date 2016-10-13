727bd1a9b7c67dfb82d30a3821d2711e128d3c7d
Maruf <kamruzzamanmaruf@gmail.com>
2016-10-13 17:33:00 +0600

    Added some more practiceing file.

---
+++ Algorithms/src/programmingInJava/Squrt.java
@@ -0,0 +1,16 @@
+package programmingInJava;
+
+
+public class Squrt {
+
+	public static void main(String[] args) {
+		double c = Double.parseDouble(args[0]);
+		double epsilon = 1e-15;
+		double t = c;
+		
+		while (Math.abs(t - c/t) > epsilon*t) {
+			t = (c/t + t) / 2.0;
+		}
+		System.out.println(t);
+	}
+}
