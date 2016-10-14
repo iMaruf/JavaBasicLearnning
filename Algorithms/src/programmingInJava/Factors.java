3f8ec126fc87f44d4aaa9255a4685e86a4eef9b8
Maruf <kamruzzamanmaruf@gmail.com>
2016-10-14 12:25:22 +0600

    Added a file which will factor given number.

---
+++ Algorithms/src/programmingInJava/Factors.java
@@ -0,0 +1,21 @@
+package programmingInJava;
+
+
+public class Factors {
+	
+	public static void main(String[] args) {
+		long n = Long.parseLong(args[0]);
+		
+		System.out.print("The primr factorization of " + n + " is: ");
+		
+		for (long factor = 2; factor*factor <= n; factor++) {
+			while (n % factor == 0) {
+				System.out.print(factor + " ");
+				n = n / factor;
+			}
+		}
+		if (n > 1)System.out.println(n);
+		else      System.out.println();
+	}
+
+}
