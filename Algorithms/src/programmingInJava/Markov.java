5f9faca3938f0c47eaa6a76e1e7053a29129d6d5
Maruf <kamruzzamanmaruf@gmail.com>
2016-10-16 09:01:13 +0600

    Updated one file.

--- Algorithms/src/programmingInJava/Markov.java
+++ Algorithms/src/programmingInJava/Markov.java
@@ -2,4 +2,37 @@ package programmingInJava;
 
 public class Markov {
 
+	public static void main(String[] args) {
+		int trials = Integer.parseInt(args[0]);
+		int n = StdIn.readInt();
+		StdIn.readInt();
+
+		double[][] p = new double[n][n];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				p[i][j] = StdIn.readDouble();
+
+		double[] rank = new double[n];
+		rank[0] = 1.0;
+		for (int t = 0; t < trials; t++) {
+
+			double[] newRank = new double[n];
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++)
+					newRank[j] += rank[k] * p[k][j];
+			}
+			rank = newRank;
+		}
+		for (int i = 0; i < n; i++) {
+			StdOut.printf("%8.5f", rank[i]);
+		}
+		StdOut.println();
+
+		StdOut.println();
+
+		for (int i = 0; i < n; i++) {
+			StdOut.printf("%2d %5.3f\n", i, rank[i]);
+		}
+	}
+
 }
